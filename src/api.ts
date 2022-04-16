7e9cdd8af5fa19c00d3abb335193fab4c18d170c
Breno Santin <brenolimaa96@gmail.com>
2022-04-16 04:06:38 -0300

    adicionando axios

--- src/api.ts
+++ src/api.ts
@@ -1,22 +1,36 @@
+import axios from 'axios';
 const BASE = 'https://jsonplaceholder.typicode.com';
 
 
 export const api = {
     getAllPosts: async () => {
-        let response = await fetch(`${BASE}/posts`);
+
+        let response = await axios.get(`${BASE}/posts`);
+        return response.data;
+
+
+        /* let response = await fetch(`${BASE}/posts`);
         let json = await response.json();
-        return json;
+        return json; */
     },
 
     addNewPost: async (title: string, body: string, userId: number) => {
-        let response = await fetch(`${BASE}/posts`, {
+
+
+        let response = await axios.post(`${BASE}/posts`, {
+            title, body, userId
+        });
+        return response.data;
+
+
+        /* let response = await fetch(`${BASE}/posts`, {
             method: 'POST',
             body: JSON.stringify({ title, body, userId}),
             headers: { 'Content-Type': 'application/json' }
         });
 
         let json = await response.json();
-        return json;
+        return json; */
     },
 
 
